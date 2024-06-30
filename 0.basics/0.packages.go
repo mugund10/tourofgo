275ddb68dea5629a8d7547d5bee7a1697fb189fb
OpenWavesGit <150226555+OpenWavesGit@users.noreply.github.com>
2024-06-30 22:51:53 +0530

    upto 1.imports

---
+++ 0.basics/0.packages.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("my favorite number is ", rand.Intn(10))
+}
+ 
---
+++ 0.basics/1.imports.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	nine := 9
+	fmt.Printf("square root of %d is %f",nine,math.Sqrt(float64(nine)))
+}
