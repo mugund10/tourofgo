d7341101a63c7f2295bc7f239cd09335913b6f0f
mugund10 <bjmugundhan@gmail.com>
2024-07-01 18:37:19 +0530

    upto : 4.variables

---
+++ 0.basics/4.variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var glob1 = 10
+var glob2 int = 12
+
+func main(){
+var vowels string = "aeiou"
+var y = 'y'
+why := "why"
+
+fmt.Println(why,vowels,"dont have",string(y))
+fmt.Printf("%d + %d = %d \n",glob1,glob2,glob1+glob2)
+
+}
