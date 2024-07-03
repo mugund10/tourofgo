5604aaabea8b01f0e9d0a806497fae35a3cc494f
mugund10 <bjmugundhan@gmail.com>
2024-07-04 00:49:31 +0530

    upto : forloops

--- 1.controlflow/0.forloops.go
+++ 1.controlflow/0.forloops.go
@@ -1,18 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	for i , j :=  3, 9  ; i ^ j > 9  ; i++{
-          fmt.Println(i,j,i ^ j )
+	for i, j := 3, 9; i^j > 9; i++ {
+		fmt.Println(i, j, i^j)
 	}
 
-	
-a := 10
+	a := 3
 
-b := 12
+	b := 12
 
-fmt.Println(a &^ b) 
+	fmt.Println(a &^ b)
+
+	// go's while loop
+
+	for a < b {
+		a += a
+	}
+
+	fmt.Println(b)
+
+// for {}
 
 }
