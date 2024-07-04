8fc7b88c4d76e94877a8efd3342d877b058a8074
mugund10 <bjmugundhan@gmail.com>
2024-07-05 02:17:11 +0530

    upto : loops and func / exercise

---
+++ 1.controlflow/1.ifcontd.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// here a is shortstatement, which means
+	// its declared inside the if condition 
+	// and cant be accessed by outside the if statement
+	if a := 10; a < 20 {
+		fmt.Println(a)
+	}
+	//fmt.Println(a) its gives error because a 
+	// initialized inside the if statement
+}
