88c1397083fe2db463fb2d68646096c50aed7b54
mugund10 <bjmugundhan@gmail.com>
2024-07-16 06:24:57 +0530

    upto range

---
+++ exercises/2.slices.go
@@ -0,0 +1,23 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+
+	sli := make([][]uint8, dy)
+
+	for keyX, _ := range sli {
+		sli1 := make([]uint8, dx)
+		for keyY, _ := range sli1 {
+			sli1[keyY] = uint8((keyX + keyY) / 2)
+		}
+		sli[keyX] = sli1
+	}
+
+	return sli
+
+}
+
+func main() {
+	pic.Show(Pic)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mugund10/tourofgo
 
 go 1.22.4
+
+require golang.org/x/tour v0.1.0
