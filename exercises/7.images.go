54b3de78275ab83768381fc792d6a89ebd23c8b3
mugund10 <bjmugundhan@gmail.com>
2024-07-21 03:07:35 +0530

    on interface

--- exercises/7.images.go
+++ exercises/7.images.go
@@ -16,7 +16,7 @@ func (i Image) ColorModel() color.Model {
 	return color.RGBAModel
 }
 func (i Image) Bounds() image.Rectangle {
-	return image.Rect(0, 0, i.width, i.height)
+	return image.Rect(0, 0, i.width, i.height
 }
 func (i Image) At(x, y int) color.Color {
 	v := uint8((x ^ y) % 256)
